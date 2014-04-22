3e84172148b3be6f62d81206f31c100b57731eab
urvashi01 <ash.khand@gmail.com>
2014-04-23 02:35:51 +0530

    Fetching and displaying ads from premium table.

--- Navigation_drawer_loctalk/src/com/loctalk/Addfragment.java
+++ Navigation_drawer_loctalk/src/com/loctalk/Addfragment.java
@@ -41,16 +41,13 @@ public class Addfragment  extends ListFragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
-		//adapter = new AdAdapter(getActivity(), llist);
-		//list = (ListView) getActivity().findViewById(android.R.id.list);
 		llist = new ArrayList<single_row>();
-//		llist.add(new single_row("hello1", "aalo le lo hajfhahfahfafhahffaf kjdfklhafa hdafakhfasf ufajfkaf lkjfajf "+"\n"+"aalo le lo hajfhahfahfafhahffaf kjdfklhafa hdafakhfasf ufajfkaf lkjfajf "));
-//		llist.add(new single_row("hello2", "gobi le lo"));
-//		llist.add(new single_row("hello3", "pyaaz le lo"));
-//		llist.add(new single_row("hello4", "tamatar le lo"));
 		ArrayList<JSONObject> array = db.getPremium();
 		int i = 0;
 		JSONObject temp = null;
+/*
+ * Fetching the records from premium table and adding them to the display list
+ */
 		while(i<=array.size()-1){
 			temp = array.get(i);
 			try{
