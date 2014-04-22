688b3ee6b0b9dab9d05873a5fa41796b8bc86751
Nikhilesh Somani <nikhilesh.somani@gmail.com>
2014-04-22 23:37:57 +0530

    chat_reQ and thoda bahut aur hai :D

---
+++ Navigation_drawer_loctalk/src/com/loctalk/chatAdapter.java
@@ -0,0 +1,110 @@
+package com.loctalk;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+public class chatAdapter extends BaseAdapter {
+	
+	private Context lcontext;
+	private Hashtable<String, ArrayList<chatObject>> chatmsg;
+	//private ArrayList<Hashtable<String, ArrayList<String>>> chat;
+	private ArrayList<String> chat;
+	
+	public chatAdapter(Context context, ArrayList<String> list, Hashtable<String, ArrayList<chatObject>> listmsg){
+		this.lcontext = context;
+		this.chat = list;
+		this.chatmsg = listmsg;
+		System.out.println("Creating adapter");
+		}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		
+		return chat.size(); 
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return chat.get(position);
+
+	}
+	
+	Context getContext(){
+		return this.lcontext;
+	}
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		View vi = convertView;
+		ViewHolder holder = null ;
+
+		System.out.println("In getView");
+		
+		if(vi==null){
+			vi = LayoutInflater.from(lcontext).inflate(R.layout.peerrow_perchat, parent, false);
+			holder = new ViewHolder();
+			holder.nickView = (TextView) vi.findViewById(R.id.nick);
+			holder.msgView = (TextView) vi.findViewById(R.id.last_chat);
+			holder.timeView = (TextView) vi.findViewById(R.id.time);
+			vi.setTag(holder);
+		}
+		else{
+			holder = (ViewHolder)vi.getTag();
+		}
+		
+		
+		  String nick = (chat.get(position));
+		  System.out.println("Nick at poistion "+position+" is "+nick);
+		  ArrayList<chatObject> ar = chatmsg.get(nick);
+		  System.out.println("First message is "+ar.get(0).getMessage());
+		  String m = (ar.get(ar.size()-1)).getMessage();
+		  if(m.length()>50){
+			  m = m.substring(0, 80);
+			  m = m + "...";
+		  }
+		  String t = (ar.get(ar.size()-1)).getTime();
+		  System.out.println("Message :"+ m);
+		  holder.nickView.setText(nick);
+          holder.msgView.setText(m);
+          holder.timeView.setText(t);
+          System.out.println("Text Set for "+position);
+            
+          return vi;
+	}
+	
+	
+	
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+}
+
+class ViewHolder{
+	TextView nickView;
+	TextView msgView;
+	TextView timeView;
+}
